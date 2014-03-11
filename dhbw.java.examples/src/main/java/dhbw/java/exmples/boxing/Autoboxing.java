ba3326a989e2f175550b0693a0fe7a73bcde01ee
deichsteller <daniel.eichsteller@gmx.de>
2014-03-11 13:45:33 +0100

    added boxing examples

---
+++ dhbw.java.examples/src/main/java/dhbw/java/exmples/boxing/Autoboxing.java
@@ -0,0 +1,18 @@
+package dhbw.java.exmples.boxing;
+
+public class Autoboxing {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Integer x = new Integer(50);
+		int y = 5;
+		int a = x; // Autoboxing
+
+		x = y + 100;
+
+	}
+
+}
