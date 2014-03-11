5f011092f2a56aa449366626eac3a71c8b984e3d
Marc <kommt-nicht-an@web.de>
2014-03-11 15:22:16 +0100

    Aufgabe9

---
+++ dhbw.java.practice/src/main/java/dhbw/java/practice/excercise9/NichtquadratischException.java
@@ -0,0 +1,7 @@
+package dhbw.java.practice.excercise9;
+
+public class NichtquadratischException extends UnloesbarException {
+    public NichtquadratischException(String message) {
+        super(message);
+    }
+}
---
+++ dhbw.java.practice/src/main/java/dhbw/java/practice/excercise9/UnloesbarException.java
@@ -0,0 +1,8 @@
+package dhbw.java.practice.excercise9;
+
+public class UnloesbarException extends Exception {
+
+    public UnloesbarException(String message) {
+        super(message);
+    }
+}
