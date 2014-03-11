228d533b0b6b5703e9f52ef7ca45b1cfc56e7e11
deichsteller <daniel.eichsteller@gmx.de>
2014-03-11 14:58:54 +0100

    test exceptions excercise not complete

---
+++ dhbw.java.examples/src/main/java/dhbw/java/examples/exceptions/MainClassTest.java
@@ -0,0 +1,15 @@
+package dhbw.java.examples.exceptions;
+
+public class MainClassTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Test t1 = new Test();
+		t1.run(0);
+		System.out.println("MainClass.");
+
+	}
+
+}
