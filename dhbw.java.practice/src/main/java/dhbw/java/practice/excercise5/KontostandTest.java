8681f77eba33496595c6bfbf8b3a65c650c5e4dd
Marc <kommt-nicht-an@web.de>
2014-03-11 13:53:44 +0100

    Benutzung IOTools in Aufgabe 6

--- dhbw.java.practice/src/main/java/dhbw/java/practice/excercise5/KontostandTest.java
+++ dhbw.java.practice/src/main/java/dhbw/java/practice/excercise5/KontostandTest.java
@@ -1,14 +1,12 @@
 package dhbw.java.practice.excercise5;
 
-//import Prog1Tools.IOTools;
+import Prog1Tools.IOTools;
 
 public class KontostandTest {
-	public static void main(String[] args) {
-		//Zahl z1 = new Kontostand(IOTools.readDouble("Kontostand 1 eingeben: "));
-		//Zahl z2 = new Kontostand(IOTools.readDouble("Kontostand 2 eingeben: "));
-		Zahl z1 = new Kontostand(5);
-		Zahl z2 = new Kontostand(6);
+    public static void main(String[] args) {
+        Zahl z1 = new Kontostand(IOTools.readDouble("Kontostand 1 eingeben: "));
+        Zahl z2 = new Kontostand(IOTools.readDouble("Kontostand 2 eingeben: "));
 
-		ZahlTest.operationenMenue(z1, z2);
-	}
+        ZahlTest.operationenMenue(z1, z2);
+    }
 }
