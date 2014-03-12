46aa9148eadef73a314d075932f652dbc996b06b
deichsteller <daniel.eichsteller@gmx.de>
2014-03-12 08:45:58 +0100

    added argument example

---
+++ dhbw.java.examples/src/main/java/dhbw/java/examples/testArray/Argument.java
@@ -0,0 +1,19 @@
+package dhbw.java.examples.testArray;
+
+public class Argument {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String str0 = args[0];
+		String str1 = args[1];
+		String str2 = args[2];
+
+		System.out.println("str0 = " + str0);
+		System.out.println("str1 = " + str1);
+		System.out.println("str2 = " + str2);
+
+	}
+
+}
