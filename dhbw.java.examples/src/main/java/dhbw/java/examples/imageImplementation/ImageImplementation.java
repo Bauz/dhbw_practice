f2c7d3f8a72a3039e54bef6d727e7fe8b7b508a1
deichsteller <daniel.eichsteller@gmx.de>
2014-03-19 09:47:54 +0100

    added default close operation

--- dhbw.java.examples/src/main/java/dhbw/java/examples/imageImplementation/ImageImplementation.java
+++ dhbw.java.examples/src/main/java/dhbw/java/examples/imageImplementation/ImageImplementation.java
@@ -1,30 +1,31 @@
 package dhbw.java.examples.imageImplementation;
 
 import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 public class ImageImplementation extends JFrame {
 
 	public ImageImplementation() {
 		
-//		Variante 1 - Bild im Projektordner
-//		BufferedImage myPicture = null;
-//		try {
-//			myPicture = ImageIO.read(new File("splash2.jpg"));
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-//		this.add(picLabel);
-//		this.setSize(960, 357);
-//		this.setVisible(true);
-		
-		// Variante 2 - Bild im Package
+		// // Variante 1 - Bild im Projektordner
+		// BufferedImage myPicture = null;
+		// try {
+		// myPicture = ImageIO.read(new File("splash2.jpg"));
+		// } catch (IOException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		// JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+		// this.add(picLabel);
+		//
+		// // Variante 2 - Bild im Package
 		// Icon icon = new ImageIcon(getClass().getResource("splash2.jpg"));
 		// JLabel lbl = new JLabel(icon);
 		// this.add(lbl);
-		// this.setSize(960, 357);
-		// this.setVisible(true);
+
+		this.setSize(960, 357);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
 	}
 
