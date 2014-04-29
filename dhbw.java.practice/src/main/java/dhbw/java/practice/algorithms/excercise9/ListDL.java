8abdfefbe9723874c9c7dbf1c997ba466d3ecbe0
Marc <kommt-nicht-an@web.de>
2014-04-29 14:04:54 +0200

    Aufgabe 9 Korrektur Doppelt Verkettete Liste

--- dhbw.java.practice/src/main/java/dhbw/java/practice/algorithms/excercise9/ListDL.java
+++ dhbw.java.practice/src/main/java/dhbw/java/practice/algorithms/excercise9/ListDL.java
@@ -2,56 +2,58 @@ package dhbw.java.practice.algorithms.excercise9;
 
 public class ListDL<T> {
 
-	static class ElementDL<E> {
-		private E element;// Inhalt des Listenelements
-		private ElementDL<E> next; // Verweis auf Nachfolger
-		private ElementDL<E> prev; // Verweis auf Vorgänger
+    private ElementDL<T> head; // Referenz auf Anfang der Liste
+    private ElementDL<T> tail; // Referenz auf Ende der Liste
 
-		public ElementDL(E o) {
-			element = o;
-			next = null;
-			prev = null;
+    public ElementDL<T> insert(T o) {
+        ElementDL<T> puffer = new ElementDL<>(o);
+        tail.next = puffer;
+        puffer.prev = tail;
+        tail = puffer;
+        return tail;
+    }
+
+    public ElementDL<T> insert(T o, ElementDL<T> pos) {
+        ElementDL<T> puffer = new ElementDL<>(o);
+        ElementDL<T> local = head;
+        while (local != pos) {
+            local = local.next;
+        }
+        puffer.next = local.next;
+        puffer.prev = local.prev;
+        local = puffer;
+        return local;
+    }
+
+    public void remove(ElementDL<T> e) {
+        ElementDL<T> local = head;
+        while (local.next != e) {
+            local = local.next;
 		}
-	}
+        local.next = local.prev;
+        local.prev = local.next;
+    }
 
-	private ElementDL<T> head; // Referenz auf Anfang der Liste
-	private ElementDL<T> tail; // Referenz auf Ende der Liste
-
-	public ElementDL<T> insert(T o) {
-		tail.next = new ElementDL(o);
-		tail = tail.next;
-		return tail;
-	}
-
-	public ElementDL<T> insert(T o, ElementDL<T> pos) {
-		ElementDL<T> local = head;
-		while (local != pos) {
-			local = local.next;
+    @Override
+    public String toString() {
+        String output = "";
+        ElementDL<T> local = head;
+        while (local != null) {
+            output = output + " " + local + ",";
+            local = local.next;
 		}
-		ElementDL<T> puffer = new ElementDL(o);
-		puffer.next = local.next;
-		local = puffer;
-		return local;
-	}
+        return output;
+    }
 
-	public void remove(ElementDL<T> e) {
-		ElementDL<T> local = head;
-		while (local.next != e) {
-			local = local.next;
-		}
-		local.next = e.next;
-		local.prev = e.prev;
-	}
+    static class ElementDL<E> {
+        private E element;// Inhalt des Listenelements
+        private ElementDL<E> next; // Verweis auf Nachfolger
+        private ElementDL<E> prev; // Verweis auf Vorgänger
 
-	@Override
-	public String toString() {
-		String output = "";
-		ElementDL<T> local = head;
-		while (local != null) {
-			output = output + " " + local + ",";
-			local = local.next;
-		}
-		return output;
+        public ElementDL(E o) {
+            element = o;
+            next = null;
+            prev = null;
+        }
 	}
-
 }
